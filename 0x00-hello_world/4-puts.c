640e5e9d6a1c3c96837b94d8cedecccdd6c75704
AnthonyyYung <anthonyakpanyung@gmail.com>
2023-02-18 07:05:52 -0800

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: a program that print using puts
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
