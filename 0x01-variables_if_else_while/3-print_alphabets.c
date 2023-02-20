057c63365132fc11f3f29ea3b88f8d124b36a0c7
AnthonyyYung <anthonyakpanyung@gmail.com>
2023-02-20 14:14:55 -0800

    C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - print alphabets in lower an dthen upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
