058623cfd7fdf688b82d76d6ce87694343095832
AnthonyyYung <anthonyakpanyung@gmail.com>
2023-02-20 15:44:16 -0800

    C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,25 +6,12 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('\n');
-	putchar('1');
-	putchar('\n');
-	putchar('2');
-	putchar('\n');
-	putchar('3');
-	putchar('\n');
-	putchar('4');
-	putchar('\n');
-	putchar('5');
-	putchar('\n');
-	putchar('6');
-	putchar('\n');
-	putchar('7');
-	putchar('\n');
-	putchar('8');
-	putchar('\n');
-	putchar('9');
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
 	putchar('\n');
 	return (0);
 }
