3a0b2434b713f71e989c83a30d7ebcc1ba284e50
AnthonyyYung <anthonyakpanyung@gmail.com>
2023-02-27 05:06:22 -0800

    C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - reassing int value with a pointer
+ *@n: integer
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+ C - Pointers, arrays and stringS
+
+This DIR holds solutions to ALX-SE tasks on Pointers , Arrays and strings 
