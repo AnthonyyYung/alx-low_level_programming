c5da8f060397120c669dc8efbde510f1b3d76f1e
AnthonyyYung <anthonyakpanyung@gmail.com>
2023-03-02 11:13:50 -0800

    C

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - copy one string into ano
+ *
+ * @src: source string
+ * @dest: destination string
+ * @n: max no. of bytes to be copied from src
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (dest[index++])
+	{
+		src_len++;
+	}
+
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	for (index = src_len; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
+}
